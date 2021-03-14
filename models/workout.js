fa6221efc158a3219e94c7edf5c95ebbe2faa03a
Van Taymany <vtaymany@gmail.com>
2021-03-13 21:46:42 -0800

    adds models

---
+++ models/workout.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      distance: {
+        type: String,
+        trim: true,
+        required: 'Distance',
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: 'Duration',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Exercise',
+      },
+      reps: {
+        type: Number,
+        trim: true,
+        required: 'Reps',
+      },
+      sets: {
+        type: Number,
+        trim: true,
+        required: 'Sets',
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: 'Type',
+      },
+      weight: {
+        type: Number,
+        trim: true,
+        required: 'Weight',
+      },
+    },
+  ],
+})
+
+const Workout = mongoose.model('Workout', workoutSchema)
+
+module.exports = Workout
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 })
 
 // Routes---app.use
-// app.use(require('./routes/api.js'))
+// app.use(require('./routes/api'))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`)
